05a56960d9b7cfa8a4acbd5bda1d328d6ca14c4b
Sauhard74 <sauhard74best@gmail.com>
2024-09-13 04:15:59 +0530

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
 
 void main() {
   runApp(const MyApp());
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
 
 class MyApp extends StatelessWidget {
